0bef84abd663c7329b97d93c9606b53a161d85bb
David Ash <david.ash@ottid.co.uk>
2023-10-04 16:39:26 +0100

    Initial Commit

---
+++ ReduxIntro/ReduxIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxIntroApp.swift
+//  ReduxIntro
+//
+//  Created by David Ash on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReduxIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
